88261005324a265fb6e54e1ad36d3a83b79b6cdb
ahsanul-hub <ahsanulwaladi@reku.id>
2023-06-24 21:55:25 +0700

    fix(vite.config) fix error build

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,34 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
-  server: {
-    fs: {
-      allow: [".."],
-    },
-  },
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-    process: { env: {} },
-  },
+// export default defineConfig({});
+
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: [{ find: "@", replacement: "/src" }],
+      },
+      server: {
+        fs: {
+          allow: [".."],
+        },
+      },
+      define: {
+        // By default, Vite doesn't include shims for NodeJS/
+        // necessary for segment analytics lib to work
+        global: {},
+        process: { env: {} },
+      },
+    };
+  }
+  if (command === "build") {
+    return {
+      plugins: [react()],
+      build: {
+        chunkSizeWarningLimit: 1000,
+      },
+    };
+  }
 });
